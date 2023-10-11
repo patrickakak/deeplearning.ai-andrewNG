1c98f3020b3e934e71073c1bc5fe37970ceb7eb1
woozway <wuzw@zju.edu.cn>
2023-10-11 19:49:35 +0800

    -

--- COURSE 1 Neural Networks and Deep Learning/Week 3/Planar data classification with one hidden layer/planar_utils.py
+++ COURSE 1 Neural Networks and Deep Learning/Week 3/Planar data classification with one hidden layer/planar_utils.py
@@ -4,10 +4,11 @@ import sklearn
 import sklearn.datasets
 import sklearn.linear_model
 
+
 def plot_decision_boundary(model, X, y):
     # Set min and max values and give it some padding
-    x_min, x_max = X[0, :].min() - 1, X[0, :].max() + 1
-    y_min, y_max = X[1, :].min() - 1, X[1, :].max() + 1
+    x_min, x_max = X[0,:].min() - 1, X[0,:].max() + 1
+    y_min, y_max = X[1,:].min() - 1, X[1,:].max() + 1
     h = 0.01
     # Generate a grid of points with distance h between them
     xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
@@ -18,7 +19,7 @@ def plot_decision_boundary(model, X, y):
     plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
     plt.ylabel('x2')
     plt.xlabel('x1')
-    plt.scatter(X[0, :], X[1, :], c=y, cmap=plt.cm.Spectral)
+    plt.scatter(X[0,:], X[1,:], c=y, cmap=plt.cm.Spectral)
     
 
 def sigmoid(x):
@@ -61,6 +62,6 @@ def load_extra_datasets():
     noisy_moons = sklearn.datasets.make_moons(n_samples=N, noise=.2)
     blobs = sklearn.datasets.make_blobs(n_samples=N, random_state=5, n_features=2, centers=6)
     gaussian_quantiles = sklearn.datasets.make_gaussian_quantiles(mean=None, cov=0.5, n_samples=N, n_features=2, n_classes=2, shuffle=True, random_state=None)
-    no_structure = np.random.rand(N, 2), np.random.rand(N, 2)
+    no_structure = np.random.rand(N,2), np.random.rand(N,2)
     
     return noisy_circles, noisy_moons, blobs, gaussian_quantiles, no_structure
